df9b8f8cdcec35154c0b600cc78be2fc297e59b7
yeonuk44 <yeonuk44@gmail.com>
2023-07-11 14:44:54 +0900

    # Add routing  setting, using nodeJS

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, World!");
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello, World!");
+  } else if (method === "GET" && url === "/about") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("About Page");
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Not Found");
+  }
 });
 
 server.listen(port, hostname, () => {
