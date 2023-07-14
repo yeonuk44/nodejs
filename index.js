dbb6f33a9ec4f76a7398341c7ce618a773ecc0ff
yeonuk44 <yeonuk44@gmail.com>
2023-07-14 17:14:37 +0900

    # Set Middleware

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const http = require("http");
+const { URL } = require("url");
+const querystring = require("querystring");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  /**
+   * INFO:
+   * Implemente routing functionality
+   */
   const { method, url } = req;
+
+  /**
+   * INFO:
+   * Implemente session functionality
+   */
+  const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+  let session = {};
+
+  console.log("parsedUrl", parsedUrl);
 
   if (method === "GET" && url === "/") {
     res.statusCode = 200;
@@ -14,10 +29,19 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.end("About Page");
-  } else {
-    res.statusCode = 404;
+  } else if (parsedUrl.pathname === "login") {
+    const { username } = querystring.parse(parsedUrl.query);
+    session.username = username;
+    const storedUsername = session.username;
+
+    res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
-    res.end("Not Found");
+    res.end(`Hello ${storedUsername}`);
+  } else if (parsedUrl.pathname === "logout") {
+    session.username = null;
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end(`Goodbye ${session.username}`);
   }
 });
 
